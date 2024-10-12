1007e25e37aa2ed8e753c73978bd85a583fd04c8
Timax <timax.met@gmail.com>
2024-10-12 20:47:03 +0300

    Final with Unit next - automapper

--- Repositories/BookRepository.cs
+++ Repositories/BookRepository.cs
@@ -13,19 +13,26 @@ namespace LibraryApp.Repositories
             _context = context;
         }
 
-        public async Task<Book> GetByIdAsync(int id)
-        {
-            return await _context.Books.Include(b => b.Author).FirstOrDefaultAsync(b => b.Id == id);
-        }
-
         public async Task<IEnumerable<Book>> GetAllAsync()
         {
             return await _context.Books.Include(b => b.Author).ToListAsync();
         }
 
+        public async Task<Book> GetByIdAsync(int id)
+        {
+            return await _context.Books.Include(b => b.Author).FirstOrDefaultAsync(b => b.Id == id);
+        }
+
+        public async Task<Book> GetByISBNAsync(string isbn)
+        {
+            return await _context.Books.Include(b => b.Author).FirstOrDefaultAsync(b => b.ISBN == isbn);
+        }
+
         public async Task AddAsync(Book book)
         {
-            await _context.Books.AddAsync(book);
+            //await _context.Books.AddAsync(book);
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
         }
 
         public void Update(Book book)
@@ -37,5 +44,30 @@ namespace LibraryApp.Repositories
         {
             _context.Books.Remove(book);
         }
+
+        public async Task BorrowBookAsync(int bookId, string userId)
+        {
+            var book = await GetByIdAsync(bookId);
+            if (book != null)
+            {
+                book.BorrowedByUserId = userId;
+                book.BorrowedAt = DateTime.Now;
+                book.ReturnAt = DateTime.Now.AddDays(14); // Устанавливаем срок возврата на 14 дней
+                _context.Books.Update(book);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task AddBookImageAsync(int bookId, string imagePath)
+        {
+            var book = await GetByIdAsync(bookId);
+            if (book != null)
+            {
+                book.ImagePath = imagePath;
+                _context.Books.Update(book);
+                await _context.SaveChangesAsync();
+            }
+        }
+
     }
 }
--- Repositories/IBookRepository.cs
+++ Repositories/IBookRepository.cs
@@ -1,13 +1,18 @@
 ﻿using LibraryApp.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace LibraryApp.Repositories
 {
     public interface IBookRepository
     {
         Task<Book> GetByIdAsync(int id);
+        Task<Book> GetByISBNAsync(string isbn); // Новый метод для получения книги по ISBN
         Task<IEnumerable<Book>> GetAllAsync();
         Task AddAsync(Book book);
         void Update(Book book);
         void Delete(Book book);
+        Task BorrowBookAsync(int bookId, string userId); // Новый метод для взятия книги на руки
+        Task AddBookImageAsync(int bookId, string imagePath); // Новый метод для добавления обложки книги
     }
 }
