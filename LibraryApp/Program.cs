b08b37fa4fbedecf923e963a5f14ec1372d11d79
Timax <timax.met@gmail.com>
2024-11-11 12:15:58 +0300

    исправления 2

--- LibraryApp/Program.cs
+++ LibraryApp/Program.cs
@@ -15,6 +15,7 @@ using LibraryApp.Interfaces;
 using LibraryApp.Mappings;
 using LibraryApp.Infrastructure;
 using LibraryApp.UseCases;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace LibraryApp
 {
@@ -157,5 +158,16 @@ namespace LibraryApp
 
             app.Run();
         }
+        public class LibraryContextFactory : IDesignTimeDbContextFactory<LibraryContext>
+        {
+            public LibraryContext CreateDbContext(string[] args)
+            {
+                var optionsBuilder = new DbContextOptionsBuilder<LibraryContext>();
+                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=LibraryDb;Trusted_Connection=True;");
+
+                return new LibraryContext(optionsBuilder.Options);
+            }
+        }
+
     }
 }
